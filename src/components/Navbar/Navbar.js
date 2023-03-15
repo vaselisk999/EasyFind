6a56c577c8d2e4e76ca32c2986d4965495bb0686
Ahmed140298 <116595107+Ahmed140298@users.noreply.github.com>
2023-03-15 21:01:25 +0000

    create navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,25 +4,30 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg">
-        <div className="container-fluid">
-            <span className="navbar-brand">EasyFind</span>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" aria-current="page" href="about">About</a>
-                </li>
-                </ul>
-            </div>
+    <div className="n-wrapper navbar navbar-expand-lg" id="Navbar">
+      <div className="n-left">
+        <div className="n-name">EasyFind</div>
+      </div>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="n-right collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="n-list">
+          <ul className="navbar-nav" style={{ listStyleType: "none"}}>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" style={{ textDecoration: "none" }} href="home">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" style={{ textDecoration: "none" }} href="/about">
+                About
+              </a>
+            </li>
+          </ul>
         </div>
-
-    </nav>
+      </div>
+    </div>
   );
 };
 
