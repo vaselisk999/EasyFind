cde1c220afddb43a2bd027a3b8048ba4f70ef01a
Ahmed140298 <116595107+Ahmed140298@users.noreply.github.com>
2023-03-16 16:43:48 +0000

    routing

--- src/components/HeaderComponent/index.js
+++ src/components/HeaderComponent/index.js
@@ -1,48 +1,41 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 import "./style.css";
-function Index() {
+import Search from "../SearchComponent/index";
 
 
+
+
+const index = () => {
   return (
-    <div className="container">
-      <header>
-        <section className="header-nav-area">
-          <div className="brand_area">
-              <span>
-                EasyFind
-              </span>
-          </div>
-          <div id="nav_container">
-            <nav id="navigation">
-              <a href="/" className="nav-element">
-                Home
-              </a>
-              <a href="about" className="nav-element">
-                About
-              </a>
-            </nav>
-          </div>
-          <span className="menu-icon"></span>
-        </section>
-          <div className="wrapper">
-            <div className="article">
-              <h1>
-                Find your book of choice.
-              </h1>
+    <div className="holder container">
+      <Navbar />
+      <div className="header">
+        <div className="row">
+          <div className="header-content p-5 flex flex-c text-center me-5 text-dark col-lg-6 col-sm-3">
+            <h2 className="header-title text-capitalize">
+              find your book of choice.
+            </h2>
+            <br />
+            <div>
               <h3>Find books with just one search!</h3>
-              <p>
-                Since 1997, BookFinder has made it easy to find any book at the best price. 
-                Whether you want the cheapest reading copy or a specific collectible edition, 
-                with BookFinder, you'll find just the right book. BookFinder.com searches the 
-                inventories of over 100,000 booksellers worldwide, accessing millions of books 
-                in just one simple step.
+              <p className="header-text fs-20 fw-3">
+                Since 2023, Easy BookFinder has made it easy to find any book at
+                the best price. Whether you want the cheapest reading copy or a
+                specific collectible edition, with BookFinder, you'll find just
+                the right book. BookFinder.com searches the inventories of over
+                100,000 booksellers worldwide, accessing millions of books in
+                just one simple step.
               </p>
             </div>
           </div>
-      </header>
+          <div className=" search-bar col ms-5 p-5 align-self-center">
+            <Search/>
+        </div>
+        </div>
+      </div>
     </div>
   );
-
 };
 
-export default Index;
+export default index;
