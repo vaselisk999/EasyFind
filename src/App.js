8657941ebc35fb10cf90383b0e5f8c9fb4271aa1
ArturK <vaselisk999@gmail.com>
2023-03-16 23:35:19 +0000

    animated images and animated list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import Header from "./components/HeaderComponent/index";
+import BooksListComponent from './components/BooksListComponent';
+import React, { useState } from 'react';
+
+
 function App() {
+  const [trigger, settriggered] = useState(false);
+
   return (
     <div className="App">
       <Header />
+
+      <div className="content-fluent">
+        <button onClick={()=>settriggered(state => !state)}>click</button>
+        <BooksListComponent triggered={trigger} />
+      </div>
     </div>
   );
 }
