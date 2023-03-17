32746060eda55f37299526a26335d39872a15681
ArturK <vaselisk999@gmail.com>
2023-03-17 19:44:01 +0000

    fixed Slide component

--- src/components/HomePageComponent/index.js
+++ src/components/HomePageComponent/index.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import BooksListComponent from './../BooksListComponent';
 import SearchComponent from "./../SearchComponent";
-
+import Slide from './Slide'
 
 
 const HomePageComponent = () => {
   const [trigger, settriggered] = useState(false);
   return (
     <div className="container-sm content-fluent">
+      <Slide />
       <SearchComponent />
-
       <button onClick={() => settriggered(state => !state)}>click</button>
       <BooksListComponent triggered={trigger} />
     </div>
