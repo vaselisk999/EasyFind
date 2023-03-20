7511afa4480c2eba9fdaa6bab5ddb8fcd6bd60f3
Zodumo <zodumom@outlook.com>
2023-03-20 02:27:58 +0000

    added some comments that will act as a guide for code

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
 import Header from "./components/HeaderComponent/index";
 import About  from "./components/HomePageComponent/index";
 import Navbar from "./components/Navbar/Navbar";
+import BooksList from './components/SearchComponent/SearchBar'; 
 
 function App() {
   return (
-    <>
+    <><>
       <Router>
         <div className="pages">
-          <Navbar/>
+          <Navbar />
           <Routes>
             <Route path="/" element={<Header />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
       </Router>
-  </>
-  );
+    </>
+    <div className="App">
+        <BooksList />
+      </div></>
+)
+
 }
 
 export default App;
