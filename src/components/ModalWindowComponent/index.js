bdb1c8a61d47ef7f24203f8b8285bda807e99f89
ArturK <vaselisk999@gmail.com>
2023-03-21 23:31:19 +0000

    fixed styles

--- src/components/ModalWindowComponent/index.js
+++ src/components/ModalWindowComponent/index.js
@@ -6,8 +6,8 @@ const ModalWindowComponent = ({number}) =>{
     console.log(number)
     return (
         <div className="modal-background">
-            <div className="book-info-wrapper"> info block</div>
-            <div className="book-viewrr-wrapper">
+            <div className="modal-book-info-wrapper"> info block</div>
+            <div className="modal-book-viewer-wrapper">
                 <BookViewer number={"0738531367"}/>
             </div>
         </div>
