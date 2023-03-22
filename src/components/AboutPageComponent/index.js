a45e4739a04dee73f61a9e14c696d20420b4cbdc
ArturK <vaselisk999@gmail.com>
2023-03-22 23:38:23 +0000

    about page text

--- src/components/AboutPageComponent/index.js
+++ src/components/AboutPageComponent/index.js
@@ -13,10 +13,10 @@ const AboutPageComponent = () => {
             Find books with just one search!
           </h3>
           <p className="header-paragraph fs-18 fw-3">
-            Since 2023, Easy BookFinder has made it easy to find any book at the
+            Since 2023, EasyFind has made it easy to find any book at the
             best price. Whether you want the cheapest reading copy or a specific
-            collectible edition, with BookFinder, you'll find just the right
-            book. BookFinder.com searches the inventories of over 100,000
+            collectible edition, with EasyFind, you'll find just the right
+            book. https://easyfindbook.netlify.app/ searches the inventories of over 100,000
             booksellers worldwide, accessing millions of books in just one
             simple step.
           </p>
